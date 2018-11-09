b51a906e3e8f0fabeb70d342ffd95357d0aff3c2
SinestroEdmonce <867208887@qq.com>
2018-11-09 11:50:27 +0800

    Little changes

--- customized_error.py
+++ customized_error.py
@@ -6,6 +6,7 @@ class CustomizedError(Exception):
     def __init__(self, error_info):
         super(CustomizedError, self).__init__()
         self.error_info = error_info
+
     def __str__(self):
         return str(self.error_info)
 
